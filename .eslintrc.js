b34f2d1e2f28e7d72393c9758a5f281fb98702e3
Yam Mesicka <yammesicka@gmail.com>
2019-07-16 23:55:57 +0300

    Add Vue + Vuetify boilerplate, try travis.yml
    - Fix babelrc problem
    - Add travis with flake8 and mypy checks

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/recommended',
+    'eslint:recommended'
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import router from './router'
+import './registerServiceWorker'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
