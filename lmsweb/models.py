038b6b1e963147d5d75ed927dd0ffc57cc436ddc
Ido Elkabetz <elkabzido@gmail.com>
2019-07-19 18:16:14 +0300

    Clean up models

--- lmsweb/models.py
+++ lmsweb/models.py
@@ -4,12 +4,9 @@ from flask_admin.contrib.peewee import ModelView
 from lmsweb import app
 
 from peewee import (
-    BooleanField,
     CharField,
     DateTimeField,
     ForeignKeyField,
-    IntegerField,
-    IntegrityError,
     Model,
     PostgresqlDatabase,
 )
@@ -30,160 +27,47 @@ class BaseModel(Model):
         database = database
 
 
+class Course(BaseModel):
+    name = CharField()
+    open_date = DateTimeField()
+
+    def get_course_administrators(self):
+        pass
+
+
 class User(BaseModel):
     username = CharField(unique=True)
     fullname = CharField()
     mail_address = CharField()
     password = CharField()
     user_type = CharField()
+    course = ForeignKeyField(Course, backref='courses', null=True)
 
-    def is_student(self):
+    @staticmethod
+    def is_student():
         return User.user_type == 'is_student'
 
-    def is_teacher(self):
+    @staticmethod
+    def is_teacher():
         return User.user_type == 'teacher'
 
     def get_user_courses(self):
-        return (
-            Course.select()
-            .join(UserCourseRelationship, on=UserCourseRelationship.course)
-            .where(UserCourseRelationship.user == self)
-            .order_by(Course.id)
-        )
-
-
-class Course(BaseModel):
-    course_id = IntegerField(unique=True)
-    name = CharField()
-    open_date = DateTimeField()
-
-    def get_course_administrators(self):
-        is_this_course = UserCourseRelationship.course == self
-        return (
-            User.select()
-            .join(UserCourseRelationship, on=UserCourseRelationship.user)
-            .where(is_this_course & UserCourseRelationship.is_admin)
-        )
-
-
-class UserCourseRelationship(BaseModel):
-    user = ForeignKeyField(User, backref='user')
-    course = ForeignKeyField(Course, backref='course')
-    is_admin = BooleanField(default=False)
+        pass
 
 
 class Lecture(BaseModel):
-    lecture_id = IntegerField(unique=True)
     course = ForeignKeyField(Course, backref='lectures')
     date = DateTimeField()
     subject = CharField()
 
     def get_participants(self):
-        return (
-            User.select()
-            .join(UserLectureRelationship, on=UserLectureRelationship.user)
-            .where(UserLectureRelationship.lecture == self)
-        )
+        pass
 
-
-class UserLectureRelationship(BaseModel):
-    user = ForeignKeyField(User, backref='user')
-    lecture = ForeignKeyField(Lecture, backref='lecture')
-
-
-# Examples
-def get_object(model, *expressions):
-    return model.get(*expressions)
-
-
-def add_course(course_id, name, open_date):
-    course = Course(course_id=course_id, name=name, open_date=open_date)
-    if course.save() != 1:
-        raise IntegrityError()
-    return course
-
-
-def add_user(username, fullname, mail_address, password, user_type):
-    user = User(**locals())
-    if user.save(force_insert=True) != 1:
-        raise IntegrityError()
-    return user
-
-
-def add_lecture(lecture_id, course_id, date, subject):
-    course = get_object(Course, Course.course_id == course_id)
-    lecture = Lecture(
-        lecture_id=lecture_id, course=course, date=date, subject=subject,
-    )
-    if lecture.save(force_insert=True) != 1:
-        raise IntegrityError()
-    return lecture
-
-
-# 1. Add a user to course
-def user_add_to_course(username, course_id, is_admin=False):
-    try:
-        user = get_object(User, User.username == username)
-        course = get_object(Course, Course.course_id == course_id)
-        with database.atomic():
-            UserCourseRelationship.create(
-                user=user, course=course, is_admin=is_admin,
-            )
-    except IntegrityError:
-        return False
-    return True
-
-
-# 2. Remove user from course
-def user_remove_from_course(username, course_id):
-    user = get_object(User, User.username == username)
-    course = get_object(Course, Course.course_id == course_id)
-    is_user_match = UserCourseRelationship.user == user
-    is_course_match = UserCourseRelationship.course == course
-    is_user_and_course_match = is_user_match & is_course_match
-    UserCourseRelationship.delete().where(is_user_and_course_match).execute()
-
-
-# 3. Get all lectures of a specific course
-def course_get_lectures(course_id):
-    return Lecture.select().join(Course).where(Course.course_id == course_id)
-
-
-# 4. Sign a user a participant in a lecture
-def user_add_to_lecture(username, lecture_id):
-    try:
-        # NOQA: TODO: Make sure user is enlisted in the lecture's course
-        user = get_object(User, User.username == username)
-        lecture = get_object(Lecture, Lecture.lecture_id == lecture_id)
-        with database.atomic():
-            UserLectureRelationship.create(user=user, lecture=lecture)
-    except IntegrityError:
-        return False
-    return True
-
-
-# 5. Get lecture participants
-def lecture_get_participants(lecture_id):
-    lecture = get_object(Lecture, Lecture.lecture_id == lecture_id)
-    return lecture.get_participants()
-
-
-# 6. Get user's courses
-def user_get_courses(username):
-    user = get_object(User, User.username == username)
-    return user.get_user_courses()
-
-
-# 7. Get course's administrators
-def course_get_administrators(course_id):
-    course = get_object(Course, Course.course_id == course_id)
-    return course.get_course_administrators()
+    def course_get_lectures(self):
+        return Lecture.select().join(Course).where(Course.course_id == self)
 
 
 ALL_MODELS = (User, Course, Lecture)
-
-# set optional bootswatch theme
-app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
 admin = Admin(app, name='LMS', template_mode='bootstrap3')
 
