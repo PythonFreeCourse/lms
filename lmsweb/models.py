afb2b48aeb2aa3b637e835b396212d03bc455126
Ido Elkabetz <elkabzido@gmail.com>
2019-08-22 07:49:08 +0300

    Clean up models and create basic matching tests

--- lmsweb/models.py
+++ lmsweb/models.py
@@ -1,31 +1,40 @@
+import enum
+import os
+
 from flask_admin import Admin
 from flask_admin.contrib.peewee import ModelView
 
 from lmsweb import app
 
 from peewee import (
-    BooleanField,
     CharField,
     DateTimeField,
     ForeignKeyField,
+    ManyToManyField,
     Model,
-    PostgresqlDatabase)
+    PostgresqlDatabase,
+    SqliteDatabase)
 
-STUDENT_ROLE = 'Student'
-STAFF_ROLE = 'Staff'
-ADMINISTRATOR_ROLE = 'Administrator'
+
+class RoleOptions(enum.Enum):
+    STUDENT_ROLE = 'Student'
+    STAFF_ROLE = 'Staff'
+    ADMINISTRATOR_ROLE = 'Administrator'
+
 
 db_config = {
     'database': app.config['DB_NAME'],
-    'user': app.config['USER'],
-    'port': app.config['PORT'],
-    'host': app.config['HOST_IP'],
-    'password': app.config['PASSWORD'],
+    'user': app.config['DB_USER'],
+    'port': app.config['DB_PORT'],
+    'host': app.config['DB_HOST_IP'],
+    'password': app.config['DB_PASSWORD'],
 }
-database = PostgresqlDatabase(**db_config)
+if app.debug:
+    database = SqliteDatabase(os.path.join(app.instance_path, 'db.sqlite'))
+elif app.env == 'production':
+    database = PostgresqlDatabase(**db_config)
 
 
-# Database models
 class BaseModel(Model):
     class Meta:
         database = database
@@ -35,13 +44,49 @@ class Course(BaseModel):
     name = CharField()
     open_date = DateTimeField()
 
+    def __str__(self):
+        return self.name
+
+    @property
+    def administrators(self):
+        admin_role = Role.get(
+            Role.name == RoleOptions.ADMINISTRATOR_ROLE.value)
+        return self.users.filter(User.role == admin_role)
+
+    @database.atomic()
+    def add_user(self, user: 'User'):
+        user.course = self
+        user.save()
+
+    @staticmethod
+    @database.atomic()
+    def remove_user(user: 'User'):
+        user.course = None
+        user.save()
+
 
 class Role(BaseModel):
     name = CharField(unique=True, choices=(
-        (ADMINISTRATOR_ROLE, ADMINISTRATOR_ROLE),
-        (STAFF_ROLE, STAFF_ROLE),
-        (STUDENT_ROLE, STUDENT_ROLE),
+        (RoleOptions.ADMINISTRATOR_ROLE.value,
+         RoleOptions.ADMINISTRATOR_ROLE.value),
+        (RoleOptions.STAFF_ROLE.value, RoleOptions.STAFF_ROLE.value),
+        (RoleOptions.STUDENT_ROLE.value, RoleOptions.STUDENT_ROLE.value),
     ))
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def is_student(self):
+        return self.name == RoleOptions.STUDENT_ROLE.value
+
+    @property
+    def is_staff(self):
+        return self.name == RoleOptions.STAFF_ROLE.value
+
+    @property
+    def is_administrator(self):
+        return self.name == RoleOptions.ADMINISTRATOR_ROLE.value
 
 
 class User(BaseModel):
@@ -50,62 +95,27 @@ class User(BaseModel):
     mail_address = CharField()
     password = CharField()
     role = ForeignKeyField(Role, backref='users')
-    is_administrator = BooleanField(default=False)
-    course = ForeignKeyField(Course, backref='users', null=True)
+    course = ForeignKeyField(Course, backref='users', null=True, unique=True)
+
+    def __str__(self):
+        return f'{self.username} - {self.fullname}'
 
 
 class Lecture(BaseModel):
-    course = ForeignKeyField(Course, backref='lectures')
-    date = DateTimeField()
     subject = CharField()
+    date = DateTimeField()
+    course = ForeignKeyField(Course, backref='lectures')
+    users = ManyToManyField(User, backref='lectures')
+
+    def __str__(self):
+        return self.subject
 
 
-ALL_MODELS = (User, Course, Lecture, Role)
+StudentLecture = Lecture.users.get_through_model()
+
+ALL_MODELS = (User, Course, Lecture, Role, StudentLecture)
 
 admin = Admin(app, name='LMS', template_mode='bootstrap3')
 
 for m in ALL_MODELS:
     admin.add_view(ModelView(m))
-
-
-def create_tables():
-    with database:
-        database.create_tables(ALL_MODELS)
-
-
-def test():
-    database.drop_tables(
-        [
-            User,
-            Course,
-            Lecture,
-            UserCourseRelationship,
-            UserLectureRelationship,
-        ],
-    )
-    create_tables()
-    from datetime import date
-
-    today = date.today()
-    add_course(course_id=1111, name='course1', open_date=today)
-    add_course(course_id=1112, name='course2', open_date=today)
-    add_course(course_id=1113, name='course3', open_date=today)
-    add_course(course_id=1114, name='course4', open_date=today)
-    add_user(  # NOQA: S106
-        username='user1',
-        fullname='User User',
-        mail_address='user@user.com',
-        password='somehash',
-        user_type='admin',
-    )
-    user2 = add_user(  # NOQA: S106, F841
-        username='user2',
-        fullname='User2 User2',
-        mail_address='user2@user2.com',
-        password='somehash2',
-        user_type='student',
-    )
-
-    add_lecture(lecture_id=992, course_id=1111, date=today, subject='lecture2')
-    add_lecture(lecture_id=993, course_id=1112, date=today, subject='lecture3')
-    add_lecture(lecture_id=994, course_id=1114, date=today, subject='lecture4')
