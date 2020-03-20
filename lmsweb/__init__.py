c570c5c16dfe18430a655d6555483eea4b6488d5
IdoElk <elkabzido@gmail.com>
2020-03-21 01:44:45 +0200

    CSRF token for login

--- lmsweb/__init__.py
+++ lmsweb/__init__.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 
+from flask_wtf.csrf import CSRFProtect
+
 project_dir = os.path.abspath(os.path.curdir)
 template_dir = os.path.join(project_dir, 'templates')
 static_dir = os.path.join(project_dir, 'static')
@@ -11,6 +13,8 @@ webapp = Flask(
     static_folder=static_dir,
 )
 webapp.config.from_pyfile('config.py')
+
+csrf = CSRFProtect(webapp)
 
 # Must import files after app's creation
 from lmsweb import views, models  # NOQA F401
