f6c2adb51351c35042b1e0ce216d91a2ce2fd5dc
Gal Singer <gal432@gmail.com>
2020-03-23 22:23:58 +0200

    get-excercises route

--- lmsweb/models.py
+++ lmsweb/models.py
@@ -20,7 +20,7 @@ from peewee import (  # type: ignore
 from playhouse.signals import Model, pre_save
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from lms.lmsweb import webapp
+from . import webapp
 
 
 class RoleOptions(enum.Enum):
