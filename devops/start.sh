aa207c95ef17d7de8ae0526323c5a207443cff5c
Gal Singer <gal432@gmail.com>
2020-03-31 02:27:27 +0300

    small fixes from bugs we found in the remote deployment

--- devops/start.sh
+++ devops/start.sh
@@ -1,4 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash
+
+if [ -f secrets.env ]; then
+  echo "Using prod secrets"
+  source secrets.env
+else
+  echo "Using default dev variables"
+fi
+
 
 SCRIPT_FILE_PATH=$(readlink -f "${0}")
 SCRIPT_FOLDER=$(dirname "${SCRIPT_FILE_PATH}")
