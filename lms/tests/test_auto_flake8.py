4ebe3d4f9cba3d1667e26bd40d2032f681cf3cdb
Gal Singer <gal432@gmail.com>
2020-04-01 08:51:11 +0300

    sample that executes

--- lms/tests/test_auto_flake8.py
+++ lms/tests/test_auto_flake8.py
@@ -1,3 +1,7 @@
+import os
+import shutil
+import tempfile
+
 from lms.lmsdb import models
 from lms.lmstests.flake8 import tasks
 
@@ -6,8 +10,36 @@ INVALID_CODE_MESSAGE = 'השתמש בצוקואים בודדים ולא בגרש
 INVALID_CODE_KEY = 'Q000'
 VALID_CODE = 'print(0)'
 
+EXECUTE_CODE = '''
+import os
+eval('os.system("touch {}")')
+'''
+
 
 class TestAutoFlake8:
+    test_directory = None
+
+    @classmethod
+    def setup_class(cls):
+        cls.test_directory = tempfile.mkdtemp()
+        cls.file_path = os.path.join(cls.test_directory, 'some-file')
+        cls.execute_script = EXECUTE_CODE.format(cls.file_path)
+
+    @classmethod
+    def teardown_class(cls):
+        if cls.test_directory is not None:
+            shutil.rmtree(cls.test_directory)
+
+    def test_pyflake_wont_execute_code(self, solution: models.Solution):
+        solution.json_data_str = self.execute_script
+        solution.save()
+        tasks.run_flake8_on_solution(solution.id)
+        comments = tuple(models.Comment.filter(models.Comment.solution == solution))
+        assert not os.listdir(self.test_directory)
+        assert len(comments) == 2
+        exec(compile(self.execute_script, '', 'exec'))
+        assert os.listdir(self.test_directory) == ['some-file']
+
     def test_invalid_solution(self, solution: models.Solution):
         solution.json_data_str = INVALID_CODE
         solution.save()
