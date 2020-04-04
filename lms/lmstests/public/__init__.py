1d86fba444318cd126581ef9b5c71a9ce87843ee
Gal Singer <gal432@gmail.com>
2020-04-04 21:00:41 +0300

    Add import of the tasks for auto resolving

--- lms/lmstests/public/__init__.py
+++ lms/lmstests/public/__init__.py
@@ -1,6 +1,11 @@
 from lms.lmstests.public.config import celery as celery_config
 from lms.lmstests.public.flake8 import tasks as flake8_tasks
+from lms.lmstests.public.identical_tests import tasks as identical_tests_tasks
 
 celery_app = celery_config.app
 
-__all__ = ('flake8_tasks', 'celery_app')
+__all__ = (
+    'flake8_tasks',
+    'celery_app',
+    'identical_tests_tasks',
+)
