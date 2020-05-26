b6cb4cbd509e964399326b76eebff74065439d9e
Yam Mesicka <yammesicka@gmail.com>
2020-05-27 01:42:57 +0300

    More refactor, start exporting routes

---
+++ lms/models/notifications.py
@@ -0,0 +1,43 @@
+import enum
+from typing import Optional
+
+from lms.lmsdb.models import Notification, User
+
+
+class NotificationKind(enum.Enum):
+    CHECKED = 1
+    FLAKE8_ERROR = 2
+
+
+def get(user):
+    return Notification.fetch(user)
+
+
+def read(user: Optional[User] = None, id_: Optional[int] = None) -> bool:
+    if id_:
+        try:
+            notification = Notification.get_by_id(id_)
+        except Notification.DoesNotExist:
+            return False
+
+        if user and (user.id != notification.user.id):
+            return False
+
+        notification.read()
+        return True
+
+    assert user, 'Must provide user or id_'  # noqa: B101, S101
+    is_success = [msg.read() for msg in Notification.fetch(user)]
+    return all(is_success)  # Not gen to prevent lazy evaluation
+
+
+def send(
+        user: User,
+        kind: NotificationKind,
+        message: str,
+        related_id: Optional[int] = None,
+        action_url: Optional[str] = None,
+) -> bool:
+    return Notification.send(
+        user, kind.value, message, related_id, action_url,
+    )
