51cecdd4feed8cd563ac31968e735812857a063f
orronai <orronai@gmail.com>
2020-09-09 09:38:19 +0300

    feat: Add user banned role and copy code button
    - Added a button to copy user's code
    - Positioned the button in the top right corner of the code
    - Changed the style of the button
    - Added in the backend a banned role
    - Added a template of banned page
    - Added a webapp flask before request checking whether a user is banned or not

--- lms/static/copy.js
+++ lms/static/copy.js
@@ -1,7 +1,12 @@
 function copy_code(button) {
     button.addEventListener('click', () => {
-        var copyText = document.getElementById("python-user-code");
+        var copyText = document.getElementById('python-user-code');
         navigator.clipboard.writeText(copyText.textContent);
+        var last = button.innerHTML;
+        button.innerHTML = 'Copied!';
+        setTimeout(function() {
+            button.innerHTML = last;
+        }, 3000);
     });
 }
 
