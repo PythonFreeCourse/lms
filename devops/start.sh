c22c6353902161047c165878b38d2ca9581b92e4
Yam Mesicka <yammesicka@gmail.com>
2020-09-28 02:18:52 +0300

    chore: VSCode debugging works now.

--- devops/start.sh
+++ devops/start.sh
@@ -12,10 +12,11 @@ SCRIPT_FILE_PATH=$(readlink -f "${0}")
 SCRIPT_FOLDER=$(dirname "${SCRIPT_FILE_PATH}")
 
 docker network create lms
-if [[ -z "${FLASK_DEBUG}" ]]; then
-  docker-compose -p lms -f "${SCRIPT_FOLDER}/lms.yml" down
-  docker-compose -p lms -f "${SCRIPT_FOLDER}/lms.yml" up -d
-else
+
+if [[ "${DEBUGGER}" == "1" || "${DEBUGGER}" = "True" ]]; then
   docker-compose -p lms -f "${SCRIPT_FOLDER}/lms.yml" -f "${SCRIPT_FOLDER}/lms.debug.yml" down
   docker-compose -p lms -f "${SCRIPT_FOLDER}/lms.yml" -f "${SCRIPT_FOLDER}/lms.debug.yml" up -d
+else
+  docker-compose -p lms -f "${SCRIPT_FOLDER}/lms.yml" down
+  docker-compose -p lms -f "${SCRIPT_FOLDER}/lms.yml" up -d
 fi
--- lms/app.py
+++ lms/app.py
@@ -1,8 +1,11 @@
 from typing import Any, Dict
 
 from lms.lmsweb import webapp
+from lms.utils import debug
 
 if __name__ == '__main__':
-    APP_CONFIG: Dict[str, Any] = {}
+    APP_CONFIG: Dict[str, Any] = {
+        'port': 8080,
+    }
 
     webapp.run(**APP_CONFIG)
