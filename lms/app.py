47d45551c9b162bf18952f1ac5f3ad0693a38ab1
Yam Mesicka <yammesicka@gmail.com>
2020-09-28 20:36:41 +0300

    build: Allow vscode debug (#187)
    * build: Allow VSCode debugging.

--- lms/app.py
+++ lms/app.py
@@ -3,6 +3,8 @@ from typing import Any, Dict
 from lms.lmsweb import webapp
 
 if __name__ == '__main__':
-    APP_CONFIG: Dict[str, Any] = {}
+    APP_CONFIG: Dict[str, Any] = {
+        'port': 8080,
+    }
 
     webapp.run(**APP_CONFIG)
