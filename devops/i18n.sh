221743d249a1339423bd85dcc348f6862967094f
Or Ronai <orronai@gmail.com>
2020-10-09 09:09:15 +0300

    - Updated the readme file - Moved the compiling to i18n.sh

--- devops/i18n.sh
+++ devops/i18n.sh
@@ -4,5 +4,6 @@ SCRIPT_FILE_PATH=$(readlink -f "${0}")
 SCRIPT_FOLDER=$(dirname "${SCRIPT_FILE_PATH}")
 MAIN_FOLDER="${SCRIPT_FOLDER}/.."
 
-echo "Running build on folder ${MAIN_FOLDER}"
-( cd "${MAIN_FOLDER}" && docker build -t lms . )
+
+echo "Compiling Flask Babel"
+pybabel compile -d "${MAIN_FOLDER}/lms/lmsweb/translations"
