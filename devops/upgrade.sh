2c948687793a9c59ec5adf294464bf96fd1ca4da
orronai <70195628+orronai@users.noreply.github.com>
2020-10-12 15:17:45 +0300

    feat: Add diskspace checkups during upgrades (#245)
    * feat: Add diskspace checkups during upgrades
    
    - Added a script in the upgrade shell file to check that there is enough free space in the partition (200MB)

--- devops/upgrade.sh
+++ devops/upgrade.sh
@@ -10,6 +10,12 @@ upgrade () {
   git pull origin
   git checkout master
 
+  USED=$(df . | awk 'NR==2{print $4}')
+  MINIMUM_MB=200
+  MINIMUM_KB=$((MINIMUM_MB * 1024))
+  if [ ${USED} -le ${MINIMUM_KB} ]; then
+      echo "There are less than ${MINIMUM_MB}MB space left in your disk. Exiting code."; return;
+  fi
 
   sudo systemctl stop nginx
   docker exec -t lms_db_1 pg_dump -c -U lmsweb lms > /home/$USER/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
