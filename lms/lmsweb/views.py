f6bee326766d1c94692df12139b814f7721b5029
DESKTOP-0SS9SLE\97254 <lena1412@gmail.com>
2020-10-17 18:22:08 +0300

    fix simplified login error condition syntax

--- lms/lmsweb/views.py
+++ lms/lmsweb/views.py
@@ -96,7 +96,7 @@ def login(login_error: Optional[str] = None):
         if user is not None and user.is_password_valid(password):
             login_user(user)
             return get_next_url(next_page)
-        elif user is None or user.is_password_valid(password) is False:
+        elif user is None or not user.is_password_valid(password):
             login_error = 'Invalid username or password'
             error_details = {'next': next_page, 'login_error': login_error}
             return redirect(url_for('login', **error_details))
