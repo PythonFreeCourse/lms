fd2232c19fe897a4513bb5358790198c16d7682d
Ron Huberfeld <32178925+ron-huberfeld@users.noreply.github.com>
2020-10-25 09:00:01 +0200

    fix: image extentions to mime types conversion.

--- lms/utils/files.py
+++ lms/utils/files.py
@@ -21,6 +21,20 @@ ALLOWED_EXTENSIONS = set(LANGUAGE_EXTENSIONS_TO_NAMES)
 
 ALLOWED_IMAGES_EXTENSIONS = {'png', 'jpeg', 'jpg', 'svg', 'tiff', 'bmp', 'ico'}
 
+IMAGES_EXTENSIONS_TO_MIME_TYPES = {
+    'png': 'image/png',
+    'jpeg': 'image/jpeg',
+    'jpg': 'image/jpeg',
+    'svg': 'image/svg+xml',
+    'tiff': 'image/tiff',
+    'bmp': 'image/bmp',
+    'ico': 'image/x-icon',
+}
+
 
 def get_language_name_by_extension(ext: str) -> str:
     return LANGUAGE_EXTENSIONS_TO_NAMES.get(ext, ext)
+
+
+def get_mime_type_by_extention(ext: str) -> str:
+    return IMAGES_EXTENSIONS_TO_MIME_TYPES.get(ext, 'image/' + ext)
