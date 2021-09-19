136ad0fc5b5e927cac1c1e9dd5e0887ff06bfccc
Or Ronai <orronai@gmail.com>
2021-09-19 12:26:23 +0300

    fix: User comments
    - Moved method to a new js file that only managers can use
    - Now users can also comment, depends on config settings of USERS_COMMENTS

---
+++ lms/static/checker.js
@@ -0,0 +1,28 @@
+function trackFinished(exerciseId, solutionId, element) {
+  element.addEventListener('click', () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', `/checked/${exerciseId}/${solutionId}`, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.responseType = 'json';
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          if (xhr.response.next !== null) {
+            window.location.href = `/view/${xhr.response.next}`;
+          } else {
+            alert("Yay! That's it!");
+          }
+        } else {
+          console.log(xhr.status);
+        }
+      }
+    };
+
+    xhr.send(JSON.stringify({}));
+  });
+}
+
+window.addEventListener('lines-numbered', () => {
+  const exerciseId = codeView.dataset.exercise;
+  trackFinished(exerciseId, window.solutionId, document.getElementById('save-check'));
+});
