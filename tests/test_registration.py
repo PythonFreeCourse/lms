43eabd3acfb6e7b8228b9cbbbc6136ff8fdbd6a3
Yam Mesicka <yammesicka@gmail.com>
2021-09-26 03:22:07 +0300

    fix: Add noqa to fake pass

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -91,7 +91,7 @@ class TestRegistration:
             'confirm': 'some_password',
         }, follow_redirects=True)
         user = User.get_or_none(User.username == 'some_user')
-        bad_token = 'fake-token43@$@'
+        bad_token = 'fake-token43@$@'  # noqa: S105
         fail_confirm_response = client.get(
             f'/confirm-email/{user.id}/{bad_token}', follow_redirects=True,
         )
