5abe6cda651324ba58976a383abed4d181e9601f
Noga Osin <noga@acumen.io>
2021-10-01 23:38:05 +0300

    minor changes

--- lms/lmsweb/forms/login.py
+++ lms/lmsweb/forms/login.py
@@ -1,6 +1,5 @@
 from flask_babel import gettext as _  # type: ignore
 from flask_wtf import FlaskForm
-from lms.lmsweb.tools.validators import UniqueUsernameRequired
 from wtforms import PasswordField, StringField
 from wtforms.fields.simple import PasswordField
 from wtforms.validators import InputRequired, Length
@@ -9,7 +8,7 @@ from wtforms.validators import InputRequired, Length
 class LoginForm(FlaskForm):
     username = StringField(
         'Username', validators=[
-            InputRequired(), UniqueUsernameRequired, Length(min=4, max=20),
+            InputRequired(), Length(min=4, max=20),
         ],
     )
     password = PasswordField(
