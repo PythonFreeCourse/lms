05f3ac054e3a2ece648e3687bcde1eca51e74b07
Yam Mesicka <yammesicka@gmail.com>
2021-10-17 02:06:10 +0300

    feature: Per course status page
    Also, lots of small improvements:
    - Hebrew strings fixes
    - Removing cluttered suggestions from views.py
    - Better migration setup - create tables that doesn't exist first
    - Tests for the status page

--- devops/bootstrap.sh
+++ devops/bootstrap.sh
@@ -4,14 +4,14 @@ SCRIPT_FILE_PATH=$(readlink -f "${0}")
 SCRIPT_FOLDER=$(dirname "${SCRIPT_FILE_PATH}")
 MAIN_FOLDER="${SCRIPT_FOLDER}/../lms"
 
-docker exec -i lms_rabbitmq_1 rabbitmqctl add_vhost lmstests-public
-docker exec -i lms_rabbitmq_1 rabbitmqctl add_vhost lmstests-sandbox
-docker exec -i lms_rabbitmq_1 rabbitmqctl set_permissions -p lmstests-public rabbit-user  ".*" ".*" ".*"
-docker exec -i lms_rabbitmq_1 rabbitmqctl set_permissions -p lmstests-sandbox rabbit-user  ".*" ".*" ".*"
-docker exec -i lms_http_1 python lmsdb/bootstrap.py
+docker exec -i lms-rabbitmq-1 rabbitmqctl add_vhost lmstests-public
+docker exec -i lms-rabbitmq-1 rabbitmqctl add_vhost lmstests-sandbox
+docker exec -i lms-rabbitmq-1 rabbitmqctl set_permissions -p lmstests-public rabbit-user  ".*" ".*" ".*"
+docker exec -i lms-rabbitmq-1 rabbitmqctl set_permissions -p lmstests-sandbox rabbit-user  ".*" ".*" ".*"
+docker exec -i lms-http-1 python lmsdb/bootstrap.py
 
 # build the image for docker inside a docker!
-docker exec lms_checks-docker-engine_1 mkdir -p /home/lms
-docker cp "${MAIN_FOLDER}"/lmstests/public/unittests/image/requirements.txt lms_checks-docker-engine_1:/home/lms/.
-docker cp "${MAIN_FOLDER}"/lmstests/public/unittests/image/Dockerfile lms_checks-docker-engine_1:/home/lms/.
-docker exec lms_checks-docker-engine_1  sh -c "cd /home/lms && docker build -t lms ."
+docker exec lms-checks-docker-engine-1 mkdir -p /home/lms
+docker cp "${MAIN_FOLDER}"/lmstests/public/unittests/image/requirements.txt lms-checks-docker-engine-1:/home/lms/.
+docker cp "${MAIN_FOLDER}"/lmstests/public/unittests/image/Dockerfile lms-checks-docker-engine-1:/home/lms/.
+docker exec lms-checks-docker-engine-1  sh -c "cd /home/lms && docker build -t lms ."
